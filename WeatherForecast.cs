089f470afe65c9b81631e6cc371de29fa2e0a949
Yuri Jordan <yurijrdn@gmail.com>
2024-12-27 18:25:48 -0300

    feat(weather): add kelvin degree

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -7,6 +7,7 @@ namespace apiClima
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureK => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
